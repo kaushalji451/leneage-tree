5f363e2e616ddee98ab321eb8b5e2026729978f3
abhishek kaushal <abhishekkaushal2526@gmail.com>
2025-04-26 13:33:35 +0530

    some change

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
     <div className='bg-yellow-100 max-sm:h-[100vh] h-full pb-20'>
        <div className='flex justify-center'>
        <div className='bg-[#3f0808] text-yellow-200 mt-20 max-sm:h-30 max-sm:w-35 h-60 w-75 rounded-2xl    flex  flex-col items-center'>
-          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR9SRRmhH4X5N2e4QalcoxVbzYsD44C-sQv-w&s" alt="" className='w-40  h-45 rounded-4xl -mt-10 max-sm:w-20 max-sm:rounded-lg max-sm:-mt-4 ' />
+          <img src="./img.jpg" alt="" className='w-40  h-45 rounded-4xl -mt-10 max-sm:w-20 max-sm:rounded-lg max-sm:-mt-4 ' />
           <p className='text-2xl font-bold max-sm:text-sm  text-center pt-6'>Alok A Triphati</p>
           <p className='text-xl max-sm:text-sm text-center'>CFO</p>
         </div>
@@ -16,12 +16,12 @@ function App() {
 
        <div className='flex justify-center  min-md:gap-20 gap-5'>
        <div className='bg-[#3f0808] text-yellow-200 mt-20 max-sm:h-30 max-sm:w-35 h-60 w-75 rounded-2xl    flex  flex-col items-center'>
-          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR9SRRmhH4X5N2e4QalcoxVbzYsD44C-sQv-w&s" alt="" className='w-40  h-45 rounded-4xl -mt-10 max-sm:w-20 max-sm:rounded-lg max-sm:-mt-4 ' />
+          <img src="./img.jpg" alt="" className='w-40  h-45 rounded-4xl -mt-10 max-sm:w-20 max-sm:rounded-lg max-sm:-mt-4 ' />
           <p className='text-2xl font-bold max-sm:text-sm  text-center pt-6'>Alok A Triphati</p>
           <p className='text-xl max-sm:text-sm text-center'>CFO</p>
         </div>
         <div className='bg-[#3f0808] text-yellow-200 mt-20 max-sm:h-30 max-sm:w-35 h-60 w-75 rounded-2xl    flex  flex-col items-center'>
-          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR9SRRmhH4X5N2e4QalcoxVbzYsD44C-sQv-w&s" alt="" className='w-40  h-45 rounded-4xl -mt-10 max-sm:w-20 max-sm:rounded-lg max-sm:-mt-4 ' />
+          <img src="./img.jpg" alt="" className='w-40  h-45 rounded-4xl -mt-10 max-sm:w-20 max-sm:rounded-lg max-sm:-mt-4 ' />
           <p className='text-2xl font-bold max-sm:text-sm  text-center pt-6'>Alok A Triphati</p>
           <p className='text-xl max-sm:text-sm text-center'>CFO</p>
         </div>
@@ -30,17 +30,17 @@ function App() {
 
         <div  className='flex justify-center px-2 min-md:gap-20 gap-5'>
         <div className='bg-[#3f0808] text-yellow-200 mt-20 max-sm:h-30 max-sm:w-35 h-60 w-75 rounded-2xl    flex  flex-col items-center'>
-          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR9SRRmhH4X5N2e4QalcoxVbzYsD44C-sQv-w&s" alt="" className='w-40  h-45 rounded-4xl -mt-10 max-sm:w-20 max-sm:rounded-lg max-sm:-mt-4 ' />
+          <img src="./img.jpg" alt="" className='w-40  h-45 rounded-4xl -mt-10 max-sm:w-20 max-sm:rounded-lg max-sm:-mt-4 ' />
           <p className='text-2xl font-bold max-sm:text-sm  text-center pt-6'>Alok A Triphati</p>
           <p className='text-xl max-sm:text-sm text-center'>CFO</p>
         </div>
         <div className='bg-[#3f0808] text-yellow-200 mt-20 max-sm:h-30 max-sm:w-35 h-60 w-75 rounded-2xl    flex  flex-col items-center'>
-          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR9SRRmhH4X5N2e4QalcoxVbzYsD44C-sQv-w&s" alt="" className='w-40  h-45 rounded-4xl -mt-10 max-sm:w-20 max-sm:rounded-lg max-sm:-mt-4 ' />
+          <img src="./img.jpg" alt="" className='w-40  h-45 rounded-4xl -mt-10 max-sm:w-20 max-sm:rounded-lg max-sm:-mt-4 ' />
           <p className='text-2xl font-bold max-sm:text-sm  text-center pt-6'>Alok A Triphati</p>
           <p className='text-xl max-sm:text-sm text-center'>CFO</p>
         </div>
         <div className='bg-[#3f0808] text-yellow-200 mt-20 max-sm:h-30 max-sm:w-35 h-60 w-75 rounded-2xl    flex  flex-col items-center'>
-          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR9SRRmhH4X5N2e4QalcoxVbzYsD44C-sQv-w&s" alt="" className='w-40  h-45 rounded-4xl -mt-10 max-sm:w-20 max-sm:rounded-lg max-sm:-mt-4 ' />
+          <img src="./img.jpg" alt="" className='w-40  h-45 rounded-4xl -mt-10 max-sm:w-20 max-sm:rounded-lg max-sm:-mt-4 ' />
           <p className='text-2xl font-bold max-sm:text-sm  text-center pt-6'>Alok A Triphati</p>
           <p className='text-xl max-sm:text-sm text-center'>CFO</p>
         </div>
