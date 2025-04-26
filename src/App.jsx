b9501333f02cb203ffe107af6ffc7844ddd02106
abhishek kaushal <abhishekkaushal2526@gmail.com>
2025-04-26 13:13:45 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,55 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+function App() {
+
+  return (
+    <>
+    <div className='bg-yellow-100 h-full pb-20'>
+       <div className='flex justify-center gap-20'>
+       <div className='bg-[#3f0808] text-yellow-200 mt-20 h-60 w-75 rounded-2xl    flex  flex-col items-center'>
+          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR9SRRmhH4X5N2e4QalcoxVbzYsD44C-sQv-w&s" alt="" className='w-40  h-45 rounded-4xl -mt-10  ' />
+          <p className='text-2xl font-bold  text-center pt-6'>Alok A Triphati</p>
+          <p className='text-xl text-center'>CFO</p>
+        </div>
+       </div>
+
+
+       <div className='flex justify-center gap-20'>
+       <div className='bg-[#3f0808] text-yellow-200 mt-30 h-60 w-75 rounded-2xl    flex  flex-col items-center'>
+          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR9SRRmhH4X5N2e4QalcoxVbzYsD44C-sQv-w&s" alt="" className='w-40  h-45 rounded-4xl -mt-10  ' />
+          <p className='text-2xl font-bold  text-center pt-6'>Abhishek Triphati</p>
+          <p className='text-xl text-center'>COO</p>
+        </div>
+        <div className='bg-[#3f0808] text-yellow-200 mt-30 h-60 w-75 rounded-2xl    flex  flex-col items-center'>
+          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR9SRRmhH4X5N2e4QalcoxVbzYsD44C-sQv-w&s" alt="" className='w-40  h-45 rounded-4xl -mt-10  ' />
+          <p className='text-2xl font-bold  text-center pt-6'>Ashutosh Triphati</p>
+          <p className='text-xl  text-center'>CTO</p>
+        </div>
+       </div>
+
+
+        <div  className='flex justify-center gap-20'>
+        <div className='bg-[#3f0808] text-yellow-200 mt-30 h-60 w-75 rounded-2xl    flex  flex-col items-center'>
+          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR9SRRmhH4X5N2e4QalcoxVbzYsD44C-sQv-w&s" alt="" className='w-40  h-45 rounded-4xl -mt-10  ' />
+          <p className='text-2xl font-bold  text-center pt-6'>Avinish Tiwari</p>
+          <p className='text-xl  text-center'>Technical devolpment Intern</p>
+        </div>
+        <div className='bg-[#3f0808] text-yellow-200 mt-30 h-60 w-75 rounded-2xl    flex  flex-col items-center'>
+          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR9SRRmhH4X5N2e4QalcoxVbzYsD44C-sQv-w&s" alt="" className='w-40  h-45 rounded-4xl -mt-10  ' />
+          <p className='text-2xl font-bold  text-center pt-6'>Abhishek kaushal</p>
+          <p className='text-xl text-center'>Technical devolpment Intern</p>
+        </div>
+        <div className='bg-[#3f0808] text-yellow-200 mt-30 h-60 w-75 rounded-2xl    flex  flex-col items-center'>
+          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR9SRRmhH4X5N2e4QalcoxVbzYsD44C-sQv-w&s" alt="" className='w-40  h-45 rounded-4xl -mt-10  ' />
+          <p className='text-2xl font-bold  text-center pt-6'>Abhishek panday</p>
+          <p className='text-xl  text-center'>Technical devolpment Intern</p>
+        </div>
+        </div>
+
+
+    </div>
+    </>
+  )
+}
+
+export default App
